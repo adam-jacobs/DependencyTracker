138be2394664c2498054e193e54b06086c9c5691
Adam Jacobs <Adam.Jacobs@cosworth.com>
2025-04-13 16:43:29 +0100

    Add boiler plate dependencies grid

--- dependency-tracker/src/pages/Dependencies.js
+++ dependency-tracker/src/pages/Dependencies.js
@@ -7,8 +7,15 @@ function Depencencies() {
 
   const [ addDependencyDialogVisible, setAddDependencyDialogVisible] = useState(false);
 
+  const projects = ["Project A", "Project B", "Project C"];
+  const data = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
   return (
-    <div className="dependencies-container">
+    <div className="page-wrapper">
       <div className="menu-bar">
         <XButton
           size={30}
@@ -16,6 +23,56 @@ function Depencencies() {
           onClick={() => setAddDependencyDialogVisible(true)}
         />
       </div>
+      {!addDependencyDialogVisible && (
+      <div className="dependencies-container">
+        <div className="selector-grid-container">
+          <div className="selector">
+            <div>
+              <label style={{fontWeight: 'bold', margin: '1rem'}}>Build:</label>
+              <br />
+                <input className="inpt" style={{margin: '1rem'}} placeholder="Enter project name"/>
+                <input className="inpt" style={{margin: '1rem'}} placeholder="Enter project version"/>
+              </div>
+          </div>
+          <div className="grid" style={{
+            display: 'grid',
+            gridTemplateColumns: `80px repeat(${projects.length}, 80px)`,
+            gridAutoRows: '80px',
+          }}>
+
+            <div />
+            
+            {projects.map((project, i) => (
+              <div style={{
+                borderRight: '1px solid black',
+                transform: 'rotate(180deg)',
+                writingMode: 'vertical-rl'
+              }}>
+                {project}
+              </div>
+            ))}
+
+            {projects.map((rowLabel, rowIndex) => (
+              <React.Fragment>
+                <div style={{
+                  borderTop: '1px solid black'
+                }}>
+                  {rowLabel}
+                  </div>
+                {data[rowIndex].map((cell, colIndex) => (
+                  <div style={{
+                    border: '1px solid black',
+                  }}>
+                    {cell}
+                  </div>
+                ))}
+              </React.Fragment>
+            ))}
+
+          </div>
+        </div>
+      </div>
+      )}
       <div className="add-dependencies-container">
         {addDependencyDialogVisible && <AddDependencyDialog onClose={() => setAddDependencyDialogVisible(false)} />}
       </div>
