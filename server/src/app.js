bb4701945984c8c3fae7c510708e38bc4b579de0
Adam Jacobs <Adam.Jacobs@cosworth.com>
2025-05-30 21:09:44 +0100

    Serve front end files from backend

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,20 @@ import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/userRoutes.js"
 import dependencyRoutes from "./routes/dependencyRoutes.js";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 app.use('/api/users', userRoutes);
 
