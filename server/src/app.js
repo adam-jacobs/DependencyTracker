958fd1bf4a1a0f917f0741cfd8cc31e92f6c313c
Adam Jacobs <53218887+adam-jacobs@users.noreply.github.com>
2025-05-31 10:14:07 +0100

    Update static path used to serve front end to account for src folder
    * Re-name client and server directories
    
    * Serve front end files from backend
    
    * Update static path used to serve front end to account for src folder
    
    ---------
    
    Co-authored-by: Adam Jacobs <Adam.Jacobs@cosworth.com>

--- server/src/app.js
+++ server/src/app.js
@@ -12,10 +12,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-})
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 
 app.use('/api/users', userRoutes);
 
