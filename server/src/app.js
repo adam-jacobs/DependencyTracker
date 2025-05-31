959821a44e06772580e887d191eeb72a767aa1c8
Adam Jacobs <Adam.Jacobs@cosworth.com>
2025-05-31 10:33:10 +0100

    Fix: Syntax error in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -18,6 +18,7 @@ app.use(express.static(path.join(__dirname, '../../client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+});
 
 app.listen(5000, () => {
   console.log("server running on localhost:5000");
